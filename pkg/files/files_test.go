9562f8e537c5a20eb72155b66999bae1e18f5573
Asaduzzaman Pavel <contact@iampavel.dev>
2022-06-14 07:04:02 +0600

    added test for pkg/files

---
+++ pkg/files/files_test.go
@@ -0,0 +1,36 @@
+package files
+
+import (
+	"io/ioutil"
+	"path"
+	"testing"
+
+	"github.com/k1ng440/rolling-hash/pkg/delta"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestReadFile(t *testing.T) {
+	tmp := path.Join(t.TempDir(), "tmp")
+	data := []byte("The quick brown fox jumps over the lazy dog") 
+	ioutil.WriteFile(tmp, data, 0644)
+
+	reader, err := ReadFile(tmp)
+	assert.NoError(t, err)
+	
+	res, err := ioutil.ReadAll(reader)
+	assert.NoError(t, err)
+
+	assert.Equal(t, data, res)
+}
+
+func TestWriteDelta(t *testing.T) {
+	deltaPath := path.Join(t.TempDir(), "signature.sig")
+
+	deltas := make(map[int]*delta.Delta)
+	for i := 0; i < 10; i++ {
+		deltas[i] = &delta.Delta{}	
+	}	
+	
+	err := WriteDelta(deltaPath, deltas)
+	assert.NoError(t, err)
+}
